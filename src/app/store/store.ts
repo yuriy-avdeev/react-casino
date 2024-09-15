e343fb20b39b68b1f70aaff794f6f18a5b1013c9
yuriy-avdeev <y.avdeiev@gmail.com>
2024-09-15 17:42:18 +0300

    chore: first commit

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,13 +1,15 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { configureStore } from "@reduxjs/toolkit"
-import { setupListeners } from "@reduxjs/toolkit/query"
 
 export const store = configureStore({
-  //
+  reducer: {
+    //
+  },
 })
 
 export type AppStore = typeof store
 export type AppDispatch = AppStore["dispatch"]
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
   RootState,
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
-import { store } from "./app/store"
+import { store } from "./app/store/store.ts"
 import "./index.css"
 
 const container = document.getElementById("root")
