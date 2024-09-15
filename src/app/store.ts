fe9f50c693d7eb483916b67b7efdcb879e6c7410
yuriy-avdeev <y.avdeiev@gmail.com>
2024-09-15 17:24:14 +0300

    first commit

---
+++ src/app/store.ts
@@ -0,0 +1,16 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+
+export const store = configureStore({
+  //
+})
+
+export type AppStore = typeof store
+export type AppDispatch = AppStore["dispatch"]
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
