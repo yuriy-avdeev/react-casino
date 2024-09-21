797b0e22dfe5834d42293a49194edd9668e1f48a
yuriy-avdeev <y.avdeiev@gmail.com>
2024-09-21 18:20:27 +0300

    feat: add  getPost

---
+++ src/app/entities/posts/api/postApi.ts
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type { IPost } from '../model/Post'
+
+export const postApi = createApi({
+  reducerPath: 'postApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode/' }),
+  endpoints: builder => ({
+    getPost: builder.query<IPost, null>({ query: () => 'posts' }),
+  }),
+})
+
+export const { useGetPostQuery } = postApi
