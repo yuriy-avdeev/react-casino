2835187b0513040a5879420572c58b540c771712
yuriy-avdeev <y.avdeiev@gmail.com>
2024-09-22 19:01:47 +0300

    feat: customize  postApi

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,10 +1,15 @@
 import type { Action, ThunkAction } from '@reduxjs/toolkit'
 import { configureStore } from '@reduxjs/toolkit'
+import { postApi } from '../entities/posts/api/postApi'
 
 export const store = configureStore({
   reducer: {
-    //
+    // Add the generated reducer as a specific top-level slice
+    [postApi.reducerPath]: postApi.reducer,
   },
+  // Adding the api middleware enables caching, invalidation, polling and other useful features of `rtk-query`.
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(postApi.middleware),
 })
 
 export type AppStore = typeof store
